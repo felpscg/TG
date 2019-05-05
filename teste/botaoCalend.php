30533f19bf0ed99a1701b4eba20de532d80c2b48
Felipe <felipefelpgomes@gmail.com>
2019-05-05 19:07:57 -0300

    05-05-19

--- teste/botaoCalend.php
+++ teste/botaoCalend.php
@@ -11,7 +11,7 @@
         <script charset='utf-8' type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js' defer='defer'></script>
         <style>
             @import url('../css/font.css');
-            
+
             #meses{
                 position: fixed;
                 margin-left: -150px;
@@ -38,25 +38,80 @@
             }
             #avancarCal{
                 position: relative;
-                
+
                 z-index:666;
             }
+/*            #tempo-IO{
+                position: fixed;
+                width: 25em;
+                height: 7em;
+                padding: 0em;
+                z-index:10777;
+                background-color: #fff !important;
+            }*/
+.campo{
+    position: fixed;
+                width: 25em;
+                height: 7em;
+                margin: 0em;
+                z-index: 777;
+                padding: 0em !important;
+                background-color: #fff !important;
+}            
+.nome-campo, .item-campo{
+    padding: 0px;
+    margin: 0px; 
+}
+p{
+padding: 0px;
+    margin: 0px; 
+    
+}
         </style>
     <body>
+        <div id="tempo-IO">
+                <div class = 'campo'>
+                    <div class = 'nome-campo'>
+                        <p><span>Horário de Entrada:</span></p>
+                        <p><span>Horário de Saída:</span></p>
+                    </div>
+                    <div class = 'item-campo'>
+                        <p><input type="time"></p>
+                        <p><input type="time"></p>
+                    </div>
+                </div>
+            </div>
         <div id="fundo-p">
-        <?php
-        require './calendario.php';
-        echo "<div id='meses'>";
-        echo "<div class='mes'>";
-        MostreCalendario('04');
-        echo "</div>";
-        echo "<div class='mes'>";
-        MostreCalendario('05');
-        echo "</div>"
-        . "</div>";
-        ?>
+            <?php
+            require './calendario.php';
+            echo "<div id='meses'>";
+            echo "<div class='mes'>";
+            $mesAtual = date("m");
+            $arrayMesAtual = str_split($mesAtual);
+            if (intval($mesAtual) < 9) {
+                $arrayMesAtual[1] ++;
+            } else if (intval($mesAtual) == 9) {
+                $arrayMesAtual[0] ++;
+                $arrayMesAtual[1] = 0;
+            } else if (intval($mesAtual) > 9 && intval($mesAtual) <= 11) {
+                $arrayMesAtual[1] ++;
+            } else if (intval($mesAtual) > 9 && intval($mesAtual) <= 11) {
+                $arrayMesAtual[0] = 0;
+                $arrayMesAtual[1] = 1;
+            }
+
+            $proximoMes = implode("", $arrayMesAtual);
+            MostreCalendario($mesAtual);
+            echo "</div>";
+            echo "<div class='mes'>";
+            MostreCalendario($proximoMes);
+
+            echo "</div>"
+            . "</div>";
+            ?>
+            
         </div>
-        
+
         <div id="fundo-s"></div>
         <button id="avancarCal">Avançar</button>
         <footer style='position:relative; top:10px; float:bottom;'>
